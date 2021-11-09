ea39cf94ef7462fcc54431b27c34493e62e8723d
miia0518 <mikhail.iakovenko@netcracker.com>
2021-11-09 17:42:47 +0300

    Create

--- src/main/java/module-info.java
+++
@@ -1,8 +0,0 @@
-module com.hospitalcrm.mainapp {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-
-    opens com.hospitalcrm.mainapp to javafx.fxml;
-    exports com.hospitalcrm.mainapp;
-}
