5ab3d95b941ed95de3a60e81a189d7222aeb5a52
miia0518 <mikhail.iakovenko@netcracker.com>
2021-11-11 17:02:06 +0300

    Create

---
+++ src/main/java/com/hospitalcrm/mainapp/HospitalCrmApplication.java
@@ -0,0 +1,13 @@
+package com.hospitalcrm.mainapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HospitalCrmApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HospitalCrmApplication.class, args);
+    }
+
+}
